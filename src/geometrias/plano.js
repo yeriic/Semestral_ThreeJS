206087154da701a81d3a9c80f71e258b0951d83a
yeriic <piratebay.swagga10@gmail.com>
2024-11-28 18:52:02 -0500

    v2.0

--- src/geometrias/plano.js
+++ src/geometrias/plano.js
@@ -1,13 +1,33 @@
 import * as THREE from 'three';
 
-const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
 const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
 export const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-const planeGeometry1 = new THREE.PlaneGeometry(10, 10);
+const planeGeometry1 = new THREE.PlaneGeometry(8.13, 4.26);
 const planeMaterial1 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
 export const plane1 = new THREE.Mesh(planeGeometry1, planeMaterial1);
 
-const planeGeometry2 = new THREE.PlaneGeometry(10, 10);
+const planeGeometry2 = new THREE.PlaneGeometry(8.13, 4.26);
 const planeMaterial2 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
 export const plane2 = new THREE.Mesh(planeGeometry2, planeMaterial2);
+
+const planeGeometry3 = new THREE.PlaneGeometry(8.13, 4.26);
+const planeMaterial3 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
+export const plane3 = new THREE.Mesh(planeGeometry3, planeMaterial3);
+
+const planeGeometry4 = new THREE.PlaneGeometry(8.13, 4.26);
+const planeMaterial4 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
+export const plane4 = new THREE.Mesh(planeGeometry4, planeMaterial4);
+
+const planeGeometry5 = new THREE.PlaneGeometry(8.13, 4.26);
+const planeMaterial5 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
+export const plane5 = new THREE.Mesh(planeGeometry5, planeMaterial5);
+
+const planeGeometry6 = new THREE.PlaneGeometry(8.13, 4.26);
+const planeMaterial6 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
+export const plane6 = new THREE.Mesh(planeGeometry6, planeMaterial6);
+
+const planeGeometry7 = new THREE.PlaneGeometry(22.91, 10.45);
+const planeMaterial7 = new THREE.MeshStandardMaterial({ color: 0xd4d9fc, side: THREE.DoubleSide });
+export const plane7 = new THREE.Mesh(planeGeometry7, planeMaterial7);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import {cube1, cube2, cube3, cube4, cube5, cube6} from './geometrias/cubo_1.js';
-import {plane, plane1, plane2} from './geometrias/plano.js';
+import {plane, plane1, plane2, plane3, plane4, plane5, plane6, plane7} from './geometrias/plano.js';
 import {ambientLight, spotLightHelper, spotLight} from './luces/luz_ambiente.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,59 +11,87 @@ const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-scene.add(cube1);
-scene.add(cube2);
-scene.add(cube3);
-scene.add(cube4);
-scene.add(cube5);
-scene.add(cube6);
 scene.add(plane);
 scene.add(plane1);
 scene.add(plane2);
+scene.add(plane3);
+scene.add(plane4);
+scene.add(plane5);
+scene.add(plane6);
+scene.add(plane7);
 scene.add(ambientLight);
 scene.add(spotLight);
 scene.add(spotLightHelper);
 
+const loader = new GLTFLoader();
+loader.load(
+  'minecraft.glb',
+  function (gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+  },
+  undefined,
+  function (error) {
+    console.error('Error al cargar el modelo:', error);
+  }
+);
+
 //posiciones
-cube1.position.y = 0.40;
+plane1.position.x = 45.05;
+plane1.position.y =  3.19;
+plane1.position.z = 59.63;
 
-cube2.position.y = -0.05;
+plane2.position.x = 44.60;
+plane2.position.y =  4.90;
+plane2.position.z = 51.26;
 
-cube3.position.z = 0.85;
+plane3.position.x = 58.91;
+plane3.position.y =  5.70;
+plane3.position.z = 51.10;
 
-cube4.position.z = 0.85;
-cube4.position.y = -0.35;
+plane4.position.x = 30.10;
+plane4.position.y =  5.70;
+plane4.position.z = 51.10;
 
-cube5.position.z = 1.5
-cube5.position.y = -0.35;
+plane5.position.x = 58.46;
+plane5.position.y =  7.90;
+plane5.position.z = 42.74;
 
-cube6.position.z = 1.5;
-cube6.position.y = -0.70;
+plane6.position.x = 29.65;
+plane6.position.y =  7.90;
+plane6.position.z = 42.74;
+
+plane7.position.x = 42.76;
+plane7.position.y =  8.99;
+plane7.position.z = 31.24;
+
 
 //rotaciones
-cube1.rotation.y = Math.PI/4;
-
-cube2.rotation.y = Math.PI/4;
-
-cube3.rotation.y = Math.PI/4;
-
-cube4.rotation.y = Math.PI/4;
-
 plane.rotation.x = -Math.PI / 2;
 plane.position.y = -0.5;
-plane1.position.z = -5 ;
-plane1.position.y = 4.5 ;
-plane2.rotation.y = -Math.PI/2; 
-plane2.position.x = -5;
-plane2.position.y = 4.5;
 
-spotLight.position.set(2, 15, 15);
+plane1.rotation.x = -Math.PI / 2;
+
+plane2.rotation.x = -Math.PI / 2;
+
+plane3.rotation.x = -Math.PI / 2;
+
+plane4.rotation.x = -Math.PI / 2;
+
+plane5.rotation.x = -Math.PI / 2;
+
+plane6.rotation.x = -Math.PI / 2;
+
+plane7.rotation.x = -Math.PI / 2;
+
+
+spotLight.position.set(2, 50, 50);
 spotLight.lookAt(cube1.position);
 spotLight.angle = Math.PI / 4; // apertura del foco
 spotLight.distance = 30;       // distancia
 spotLight.penumbra = 0.5;      // bordes
 
-camera.position.set(2, 5, 10);
+camera.position.set(75, 75, 75);
 camera.lookAt(cube1.position);
 
 const controls = new OrbitControls(camera, renderer.domElement);
